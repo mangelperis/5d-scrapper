2f0e18294459154c23da677a3e0e0eb84812d2c6
mangelperis <mapi_meliana@hotmail.com>
2024-05-02 02:02:33 +0200

    Refactor try..catch

--- app/src/Infrastructure/UI/Controller/ProjectViewController.php
+++ app/src/Infrastructure/UI/Controller/ProjectViewController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Infrastructure\UI\Controller;
 use App\Application\Service\ProjectService;
 use Exception;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,7 @@ class ProjectViewController extends AbstractController
 {
     public function __construct(
         private ProjectService $projectService,
+        private LoggerInterface $logger,
     )
     {
     }
@@ -23,12 +25,19 @@ class ProjectViewController extends AbstractController
     #[Route('/projects', name: 'app_project_list', methods: ['GET'])]
     public function index(): Response
     {
-        $projects = $this->projectService->findAll();
+        try {
+            $projects = $this->projectService->findAll();
 
-        return $this->render('project/list.html.twig', [
-            'title' => sprintf("List of %d projects", count($projects)),
-            'projects' => $projects,
-        ]);
+            return $this->render('project/list.html.twig', [
+                'title' => sprintf("List of %d projects", count($projects)),
+                'projects' => $projects,
+            ]);
+        } catch (Exception $exception) {
+            $this->logger->error("[API] Projects list error: {$exception->getMessage()}");
+            //TODO Create Error Response Handler
+            return new Response('Something went wrong');
+        }
+
     }
 
     /**
@@ -37,17 +46,23 @@ class ProjectViewController extends AbstractController
     #[Route('/project/{projectId}', name: 'app_project_detail', methods: ['GET'])]
     public function projectDetail(int $projectId): Response
     {
-        $project = $this->projectService->findProjectById($projectId);
+        try {
+            $project = $this->projectService->findProjectById($projectId);
 
-        $QR = $this->projectService->generateQR($project->getUrl());
+            $QR = $this->projectService->generateQR($project->getUrl());
 
-        //Hit count++
-        $this->projectService->increaseHitCount($project);
+            //Hit count++
+            $this->projectService->increaseHitCount($project);
 
-        return $this->render('project/detail.html.twig', [
-            'title' => $project->getTitle(),
-            'project' => $project,
-            'QR' => $QR
-        ]);
+            return $this->render('project/detail.html.twig', [
+                'title' => $project->getTitle(),
+                'project' => $project,
+                'QR' => $QR
+            ]);
+        } catch (Exception $exception) {
+            $this->logger->error("[API] Project detail error: {$exception->getMessage()}");
+            //TODO Create Error Response Handler
+            return new Response('Something went wrong');
+        }
     }
 }
